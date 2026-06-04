f19456ec5e68e5d4e950ff905ed5eba997590029
Lokesh S <lokeshsridhar2002@gmail.com>
2026-06-04 22:30:21 +0530

    updated back end url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https:backend-focus-seu6.onrender.com' || import.meta.env.VITE_API_URL ,
+  baseURL: 'https://backend-focus-seu6.onrender.com' || import.meta.env.VITE_API_URL ,
   timeout: 15000,
 });
 
